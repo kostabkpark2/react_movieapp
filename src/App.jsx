5af79acfabdf2753a77cb21f9c625ec9ded00728
kostabkpark2 <kostabkpark2@gmail.com>
2025-11-06 10:21:49 +0900

    movieapp - step 1

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,15 @@ function App() {
   const [movies, setMovies] = useState([]);
   
   useEffect(() => {
-    // 1. async 함수 정의
+    // 1-1. async 함수 정의
     const getMovies =  async () => {
-    // 2. await 를 사용해 비동기 작업 수행
-    const res = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=7.5&sort_by=year");
+    // 1-2. await 를 사용해 비동기 작업 수행
+    const res = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8&sort_by=year");
     const json = await res.json();
     setMovies(json.data.movies);
     setIsLoading(false);
   };
-  // 3. 정의된 함수를 즉시 호출
+  // 2. 정의된 함수를 즉시 호출
   getMovies();
   }, []); 
 
@@ -22,11 +22,18 @@ function App() {
       <div>
        <h1>Movie App</h1>
        {isLoading ? <h2>Loading...</h2> :
-                    <ul>
-                      {movies.map((movie)=>
-                        <li key={movie.id}>{movie.title}</li>
+                    <div>{movies.map((movie)=>
+                        <div key={movie.id}>
+                          <img src={movie.medium_cover_image} alt="medium_cover_image" />
+                          <h2>{movie.title}</h2>
+                          <p>{movie.summary}</p>
+                          <ul>
+                            {movie.genres.map((genre, index)=>
+                            <li key={index}>{genre}</li>)}
+                          </ul>
+                        </div>
                       )}
-                    </ul>
+                    </div>
        }
       </div>
   )
