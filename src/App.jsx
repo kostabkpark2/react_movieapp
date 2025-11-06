ee9b7057733c4ad04dccb740e29aa459b583e151
kostabkpark2 <kostabkpark2@gmail.com>
2025-11-06 10:01:10 +0900

    movieapp - async await

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import './App.css'
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  const getMovies = () => {
-    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=7.5&sort_by=year")
-      .then((res)=> res.json())
-      .then((json)=>{
-        setMovies(json.data.movies);
-        setIsLoading(false);
-        //console.log(json);
-      });
+  
+  useEffect(() => {
+    // 1. async 함수 정의
+    const getMovies =  async () => {
+    // 2. await 를 사용해 비동기 작업 수행
+    const res = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=7.5&sort_by=year");
+    const json = await res.json();
+    setMovies(json.data.movies);
+    setIsLoading(false);
   };
-  useEffect(getMovies, []);
+  // 3. 정의된 함수를 즉시 호출
+  getMovies();
+  }, []); 
 
   return (
       <div>
